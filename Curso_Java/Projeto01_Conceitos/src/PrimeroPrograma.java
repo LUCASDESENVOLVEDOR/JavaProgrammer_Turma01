f3141082fa7438beca9a35df5045e72aec06963b
Lucas Aragão Feliciano <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2023-01-10 20:18:59 -0300

    AULA 01 - PARTE 1

---
+++ Curso_Java/Projeto01_Conceitos/src/PrimeroPrograma.java
@@ -0,0 +1,14 @@
+
+
+//{} bloco.
+
+
+public class PrimeroPrograma {	
+	//metodo main!	
+	//main = ctrl + espaço e ENTER.		
+	//main bloco principal.
+	public static void main(String[] args) {		
+		System.out.println("Conceitos de Java");		
+	}	
+	//ctrl + s = SALVA O NOSSO CODIGO.
+}
