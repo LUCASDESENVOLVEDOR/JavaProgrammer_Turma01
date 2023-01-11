addac9e00868dbfa9f7ff682214b9b6642f33d09
Lucas Aragão Feliciano <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2023-01-10 21:28:05 -0300

    Add files via upload

---
+++ Curso_Java/Projeto01_Conceitos/src/br/com/impacta/PrimeroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.impacta;
+
+
+//{} bloco.
+
+
+public class PrimeroPrograma {	
+	//metodo main!	
+	//main = ctrl + espaço e ENTER.		
+	//main bloco principal.
+	public static void main(String[] args) {		
+		System.out.println("Conceitos de Java");		
+	}	
+	//ctrl + s = SALVA O NOSSO CODIGO.
+}
---
+++ Curso_Java/Projeto01_Conceitos/src/br/com/impacta/TerceiroPrograma.java
@@ -0,0 +1,21 @@
+package br.com.impacta;
+
+import javax.swing.JOptionPane;
+
+public class TerceiroPrograma {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog
+		(null,
+                          "Importe de "
+                          + "classes"
+                          + "              "
+                          + " de"
+                          + " pacotes");
+		
+		
+		
+	}
+	
+}
