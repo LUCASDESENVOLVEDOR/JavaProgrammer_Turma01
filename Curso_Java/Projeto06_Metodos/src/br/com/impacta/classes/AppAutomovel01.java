53c42f83e7bef19b7e639116a3f72eded12b3f6c
Lucas Aragão Feliciano <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2023-02-14 21:31:10 -0300

    Add files via upload

--- Curso_Java/Projeto06_Metodos/src/br/com/impacta/classes/AppAutomovel01.java
+++ Curso_Java/Projeto06_Metodos/src/br/com/impacta/classes/AppAutomovel01.java
@@ -9,16 +9,37 @@ public class AppAutomovel01 {
 		//declarar variavel.
 		
 		//new responsavel por criar o objeto;
-		Automovel auto = new Automovel();	   
-		auto.lerDados("Honda","Civic",2010,"EBX1234");	
+		Automovel auto = new Automovel();	
 		
-		String texto = auto.mostrar();
+		auto.lerDados("Honda","Civic",2020,"EBX1234");	
+		
+	
+		
+		//auto.setAno(1959);
+		
+		String texto = auto.mostrar("titulo personalizado");
 		
 		System.out.println(texto);
-		JOptionPane.showMessageDialog(null, texto);
+		JOptionPane.showMessageDialog(null, texto,"Relátorio", JOptionPane.ERROR_MESSAGE);
 		
-		Automovel auto2 = new Automovel();
-		auto2.lerDados("Chevrolet","Corsa",2023,"AAA0000");		
+		int n = Integer.parseInt(
+				JOptionPane.showInputDialog("Informe um número"));
+		
+		
+		
+		//Automovel auto2 = new Automovel();
+		//auto2.lerDados("Chevrolet","Corsa",2023,"AAA0000");		
+		
+		//texto = auto2.mostrar();
+		
+		//System.out.println(texto);
+		//JOptionPane.showMessageDialog(null, texto);
+		
+		
+	
+		
+		
+		
 		
 		
 		
--- Curso_Java/Projeto06_Metodos/src/br/com/impacta/classes/Automovel.java
+++ Curso_Java/Projeto06_Metodos/src/br/com/impacta/classes/Automovel.java
@@ -6,20 +6,76 @@ public class Automovel {
 //atributos.
  private String	marca;
  private String	modelo;
- private int	    ano;
+ private int	 ano;
  private String	placa;
  
+ 
+	public String getMarca() {
+	return marca;
+    }
+
+    public void setMarca(String marca) {
+	this.marca = marca;
+   }
+
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		
+		if(ano < 1970) {			
+			throw new NumberFormatException("O ano não pode ser menor que 1970.");
+		}
+		
+		this.ano = ano;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
 	public String mostrar() {
 		
-		String resposta = "Marca: " + marca + 
-				          "\nModelo: " + modelo + 
-				          "\nAno: " + ano + 
-				          "\nPlaca:" + placa;
+		String titulo = "DADOS DO AUTOMÓVEL";
 		
-		return resposta;
+		String resposta = titulo + "\n\nMarca: " + this.getMarca() + 
+				          "\nModelo: " + this.getModelo() + 
+				          "\nAno: " + this.getAno() + 
+				          "\nPlaca:" + this.getPlaca();
+	
+	return resposta;
 		
 		
 	}
+	
+    public String mostrar(String titulo) {
+		
+		
+		String resposta = titulo + "\n\nMarca: " + this.getMarca() + 
+				          "\nModelo: " + this.getModelo() + 
+				          "\nAno: " + this.getAno() + 
+				          "\nPlaca:" + this.getPlaca();
+	
+	return resposta;
+		
+		
+	}
+	
+   
+   
 
 	public void lerDados(String marca, String modelo, int ano, String placa)
 	{
@@ -27,12 +83,17 @@ public class Automovel {
 		
 		//this referencia da propria classe.
 		
-		this.marca = marca;
-		this.modelo = modelo;
-		this.ano = ano;
-		this.placa = placa;
+		this.setMarca(marca); 
+		this.setModelo(modelo);
+		this.setAno(ano);
+		this.setPlaca(placa); 
 	}
 
+	
+	public static double somar (double x, double y) {
+		return x + y;
+	}
+	
 	
 
 }
